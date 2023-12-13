6c2a9ae7ae975484609e54cc1cc5a10283d00539
Michael Dietz <mrdietz@gmail.com>
2023-12-12 23:45:25 -0600

    Completed exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...items) {
+    for (let i = 0; i <= items.length; i++) {
+       if (array.includes(items[i])) {
+        array.splice(array.indexOf(items[i]), 1)
+       };
+    }
+    return array;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
