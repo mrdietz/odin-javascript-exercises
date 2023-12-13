c03d2fafdec6f51862f9e044e63b8ec17abc935e
Michael Dietz <mrdietz@gmail.com>
2023-12-12 21:14:41 -0600

    Completed exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let str = '';
+    if (num < 0) {
+        return "ERROR";
+    }
+    while (num > 0) {
+        str += string;
+        num--;
+    }
+    return str;
 };
 
 // Do not edit below this line
