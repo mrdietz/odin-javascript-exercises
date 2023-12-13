fd62c4da0345f185c83437bf5c9c007f88ce2407
Michael Dietz <mrdietz@gmail.com>
2023-12-13 00:29:17 -0600

    Completed exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let endSum = 0;
+    if (a < 0 || b < 0 || typeof a !== "number" || typeof b !== "number" ) {
+        return "ERROR";
+    } else if (a > b) {
+        while (a >= b) {
+            endSum += a;
+            a--;
+            }
+    } else {
+        while (b >= a) {
+            endSum += b;
+                b--;
+            }
+    } return endSum;
+}; 
 
-};
+console.log(sumAll(1, 1));
 
 // Do not edit below this line
 module.exports = sumAll;
